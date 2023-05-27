35e7187ba0e83a48b6da45f4ec53229e530741f0
clevermaxx <130814523+clevermaxx@users.noreply.github.com>
2023-05-27 07:15:28 +0300

    Kiritish-chiqarish
    1-masala

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a, P;
+			a = Convert.ToInt32(Console.ReadLine());
+			P = 4 * a;
+			Console.WriteLine(P);
+			Console.ReadKey();
+			int b, c, d;
+			c = Convert.ToInt32(Console.ReadLine());
+			d = Convert.ToInt32(Console.ReadLine());
+			b = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine(d);
+
+		}
+	}
+}
