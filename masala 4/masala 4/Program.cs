f4ae553b74033d01619d994251300efc92fce8c5
clevermaxx <130814523+clevermaxx@users.noreply.github.com>
2023-06-09 10:06:29 +0500

    Add files via upload
    masala4

---
+++ masala 4/masala 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masala_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double d;
+            int L;
+            d = Convert.ToDouble(Console.ReadLine());
+            L = Convert.ToInt32(Math.PI * d);
+            Console.WriteLine(L);
+            Console.ReadKey();
+            
+        }
+    }
+}
