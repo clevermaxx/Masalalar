0f3fc52d2899cb94ab665fda8c53f6c56f6a6f24
clevermaxx <130814523+clevermaxx@users.noreply.github.com>
2023-06-09 10:07:17 +0500

    Add files via upload
    masala5

---
+++ masala5/masala5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masala5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kubning hajmi va to'la sirtini topish
+
+            int a;
+            double V, S;
+            a = Convert.ToInt32(Console.ReadLine());
+            V = Math.Pow(a, 3);
+            S = 6 * Math.Pow(a, 2);
+            Console.WriteLine(V);
+            Console.WriteLine(S);
+            Console.ReadKey();
+
+
+        }
+    }
+}
