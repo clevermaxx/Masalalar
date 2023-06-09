efe1595dd5f644a37d07d09a0a2f3d34351c86cc
clevermaxx <130814523+clevermaxx@users.noreply.github.com>
2023-06-09 10:08:59 +0500

    Add files via upload
    masala 7

---
+++ masala 7/masala 7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masala_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Doiraning uzunligi va yuzasini topish;
+            int R;
+            Double L, S;
+            R = Convert.ToInt32(Console.ReadLine());
+            L = 2 * Math.PI * R;
+            S = Math.PI * Math.Pow(R, 2);
+            Console.WriteLine(L);
+            Console.WriteLine(S);
+            Console.ReadKey();
+
+        }
+    }
+}
