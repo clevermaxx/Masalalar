8e88493d30f926ba0530f771c3497fdebe9a87bb
clevermaxx <130814523+clevermaxx@users.noreply.github.com>
2023-06-09 10:10:25 +0500

    Add files via upload
    masala 9

---
+++ masala9/masala9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masala9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ikkita sonning o'rta geometrigi aniqlansin
+            int a, b;
+            double geometrigi;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            geometrigi = Math.Sqrt(a * b);
+            Console.WriteLine(geometrigi);
+            Console.ReadKey();
+
+        }
+    }
+}
