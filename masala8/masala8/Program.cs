1ffdf692d8ec9ec0ab1a864a6ea736505eacfc5c
clevermaxx <130814523+clevermaxx@users.noreply.github.com>
2023-06-09 10:09:47 +0500

    Add files via upload
    masala 8

---
+++ masala8/masala8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masala8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ikkita sonning o'rta arifmetigi aniqlansin;
+
+            int a, b, arifmetigi;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            arifmetigi = (a + b) / 2;
+            Console.WriteLine(arifmetigi);
+            Console.ReadKey();
+
+        }
+    }
+}
