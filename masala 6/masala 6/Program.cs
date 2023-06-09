9d424e7191b3177f2ccde52476e847dd1c295505
clevermaxx <130814523+clevermaxx@users.noreply.github.com>
2023-06-09 10:08:12 +0500

    Add files via upload
    masala6

---
+++ masala 6/masala 6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masala_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Paralelepipedning hajmi va to'la sirtini topish.
+            int a, b, c, V, S;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            V = a * b * c;
+            S = 2 * (a * b + b * c + a * c);
+            Console.WriteLine(V);
+            Console.WriteLine(S);
+            Console.ReadKey();
+
+        }
+    }
+}
